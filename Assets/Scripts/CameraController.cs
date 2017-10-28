787531e7d704260574cbe2097e63782cf1246a36
hanjianqiao <hanjianqiao@gmail.com>
2017-10-28 17:29:20 +0800

    touch control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,11 +10,24 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+		//offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + offset;
+		//transform.position = player.transform.position + offset;
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+		{
+			Vector2 touchPosition = Input.GetTouch(0).position;
+			double halfScreen = Screen.width / 2.0;
+
+			//Check if it is left or right?
+			if(touchPosition.x < halfScreen){
+				transform.Translate(Vector3.left * 5 * Time.deltaTime);
+			} else if (touchPosition.x > halfScreen) {
+				transform.Translate(Vector3.right * 5 * Time.deltaTime);
+			}
+
+		}
 	}
 }
