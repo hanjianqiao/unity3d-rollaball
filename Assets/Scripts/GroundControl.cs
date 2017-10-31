e30f1bf623d078f212b94a8facfcd9ba4a2f02aa
hanjianqiao <hanjianqiao@gmail.com>
2017-10-31 21:55:58 +0800

    rotate

---
+++ Assets/Scripts/GroundControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundControl : MonoBehaviour {
+	
+	public float angle;
+
+	public Vector3 point, axis;
+
+	// Use this for initialization
+	void Start () {
+		transform.RotateAround (point, axis, angle);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+	}
+}
